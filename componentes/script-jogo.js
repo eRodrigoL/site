b37b39e2e0d46cd1413cf813d06556f06c7fb827
Emerson Rodrigo Lopes <e.rodrigo.l@outlook.com>
2024-04-07 03:21:55 -0300

    Adicionados os demias gráficos em jogo.html

--- componentes/script-jogo.js
+++ componentes/script-jogo.js
@@ -71,3 +71,103 @@ var avaliacao = new Chart(ctx, {
     data: dados,
     options: opcoes
 });
+
+
+
+//..........GRÁFICO ESTILO..........
+// Dados para o gráfico
+var dados = {
+    labels: ["Sorte", "Azar", "Destreza", "Outros"],
+    datasets: [{
+        label: "Estilo",
+        data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(105, 105, 105, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(105, 105, 105, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+  
+// Configurações do gráfico
+var opcoes = {
+    scales: {
+        yAxes: [{
+            display: false
+        }],
+        xAxes: [{
+            display: false
+        }]
+    }
+};
+  
+// Criar o gráfico de pizza
+var ctx = document.getElementById('estilo').getContext('2d');
+var estilo = new Chart(ctx, {
+    type: 'doughnut',
+    data: dados,
+    options: opcoes
+});
+
+
+
+//..........GRÁFICO JOGABILIDADE..........
+// Dados para o gráfico
+var dados = {
+    labels: ["Rolagem de Dados", "Leilão", "Desenho", "Posicionamento de Dados", "Construção de Mapa", "Administração de Recursos", "Locação de Trabalhadores", "Mira", "Uso e Descarte de Cartas"],
+    datasets: [{
+        label: "Jogabilidade",
+        data: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+  
+// Configurações do gráfico
+var opcoes = {
+    scales: {
+        yAxes: [{
+            display: false
+        }],
+        xAxes: [{
+            display: false
+        }]
+    }
+};
+  
+// Criar o gráfico de pizza
+var ctx = document.getElementById('jogabilidade').getContext('2d');
+var estilo = new Chart(ctx, {
+    type: 'pie',
+    data: dados,
+    options: opcoes
+});
