6135b57d35d2c198422fc6b1c82aebd70830d0f1
thais <thaismferreira42@gmail.com>
2024-04-11 21:24:32 -0300

    campos da tabela de usuário corrigidos e adicionado alerta para senhas diferentes

--- frontend/componentes/script-usuarios.js
+++ frontend/componentes/script-usuarios.js
@@ -1,4 +1,7 @@
-// 3- Cadastrando usuários
+const senha = document.getElementById('senha-usuario');
+const confirmacao = document.getElementById('confirmacao');
+
+// Cadastrando usuários
 
 document.getElementById('cadastroUsuarios').addEventListener('submit', async function(event) {
     event.preventDefault();
@@ -9,27 +12,35 @@ document.getElementById('cadastroUsuarios').addEventListener('submit', async fun
         userData[key] = value;
     });
 
-    try {
-        const response = await fetch('http://localhost:3000/api/usuarios/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        });
+    if (senha.value == confirmacao.value){
 
-        if (!response.ok) {
-            //return conteudoCadastrar.innerHTML = 'Erro ao cadastrar usuário!'
-            throw new Error('Erro ao cadastrar usuário');
+        try {
+            const response = await fetch('http://localhost:3000/api/usuarios/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+    
+            if (!response.ok) {
+                //return conteudoCadastrar.innerHTML = 'Erro ao cadastrar usuário!'
+                throw new Error('Erro ao cadastrar usuário');
+            }
+            //return conteudoCadastrar.innerHTML = 'Usuário cadastrado com sucesso!' 
+            alert('Usuário cadastrado com sucesso!');
+            // Limpar o formulário após o cadastro
+            this.reset();
+        } catch (error) {
+            console.error('Erro:', error);
+            //return conteudoCadastrar.innerHTML = 'Erro ao cadastrar usuário! Tente novamente'
+            alert('Ocorreu um erro ao cadastrar o usuário. Por favor, tente novamente mais tarde.');
         }
-        //return conteudoCadastrar.innerHTML = 'Usuário cadastrado com sucesso!' 
-        alert('Usuário cadastrado com sucesso!');
-        // Limpar o formulário após o cadastro
-        this.reset();
-    } catch (error) {
-        console.error('Erro:', error);
-        //return conteudoCadastrar.innerHTML = 'Erro ao cadastrar usuário! Tente novamente'
-        alert('Ocorreu um erro ao cadastrar o usuário. Por favor, tente novamente mais tarde.');
     }
+    else{
+        alert('As senhas não correspondem, por favor tentar novamente!');
+    }
+
+    
 });
 
