fafad9a54ceafa7de3ed0bdabe9a09dc837146ef
thais <thaismferreira42@gmail.com>
2024-05-19 16:37:59 -0300

    inserindo alteração do formulario de cadastro de usuários (foto)

--- frontend/componentes/script-usuarios.js
+++ frontend/componentes/script-usuarios.js
@@ -1,17 +1,16 @@
-document.getElementById('cadastroUsuarios').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Evita o comportamento padrão do formulário
 
-    // Obter os valores do formulário
+document.getElementById('cadastroUsuarios').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
     const nome = document.getElementById('nome-usuario').value;
     const apelido = document.getElementById('apelido-usuario').value;
     const nascimento = document.getElementById('nascimento-usuario').value;
     const email = document.getElementById('email-usuario').value;
     const senha = document.getElementById('senha-usuario').value;
     const confirmacao = document.getElementById('confirmacao').value;
-    const foto = document.getElementById('foto-usuario').files[0]; // apenas o primeiro arquivo
 
-    // Validar se os campos obrigatórios estão preenchidos
-    if (!nome || !apelido || !email || !senha || !confirmacao) {
+       // Validar se os campos obrigatórios estão preenchidos
+       if (!nome || !apelido || !email || !senha || !confirmacao) {
         alert('Por favor, preencha todos os campos obrigatórios.');
         return;
     }
@@ -22,39 +21,35 @@ document.getElementById('cadastroUsuarios').addEventListener('submit', async fun
         return;
     }
 
-    // Criar um objeto FormData para enviar os dados do formulário
-    const formData = new FormData();
-    formData.append('nome', nome);
-    formData.append('apelido', apelido);
-    formData.append('nascimento', nascimento);
-    formData.append('email', email);
-    formData.append('senha', senha);
-
-    // Verificar se foi selecionada uma foto
-    if (foto) {
-        formData.append('file', foto);
-    }
-    else{
-        formData.append('file', "#");
-    }
-
-    // Enviar a requisição POST para a API
-    try{
-        const response = await fetch('http://localhost:3000/api/usuarios', {
-        method: 'POST',
-        body: formData
-    });
-
-    if (!response.ok) {
-        //throw new Error('O seu email ou apelido já está em uso, por gentileza alterar!');
-        return alert('O seu email ou apelido já está em uso, por gentileza alterar!');
+    const dados = {
+        nome: nome,
+        apelido: apelido,
+        nascimento: nascimento,
+        email: email,
+        senha: senha
     };
 
-    alert('Usuário cadastrado com sucesso!');
-    document.getElementById('cadastroUsuarios').reset();
+    try {
+        const response = await fetch('http://localhost:3000/api/usuarios', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dados)
+        });
 
-    } catch(error){
-        console.log(error);
-        alert('Teste');
+        if (!response.ok) {
+           return alert('O apelido ou email já está em uso!');
+        }
+
+        //const resultado = await response.json();
+        return alert('Usuário cadastrado com sucesso!');
+        //console.log(resultado);
+        
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao cadastrar usuário');
     }
-}); 
+});
+
+
