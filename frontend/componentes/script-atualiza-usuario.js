8b4a2a6e04870ae577a410d32979580aea162748
thais <thaismferreira42@gmail.com>
2024-06-16 22:10:00 -0300

    adicionando alterações feitas no update do usuario

--- frontend/componentes/script-atualiza-usuario.js
+++ frontend/componentes/script-atualiza-usuario.js
@@ -1,14 +1,12 @@
 function formatarData(data) {
-     // Extrai apenas a parte da data (ano, mês e dia) da string
-     const [ano, mes, dia] = data.split('T')[0].split('-');
-     return `${ano}-${mes}-${dia}`;
+    // Extrai apenas a parte da data (ano, mês e dia) da string
+    const [ano, mes, dia] = data.split('T')[0].split('-');
+    return `${ano}-${mes}-${dia}`;
 }
 
-
 // Preenche os dados no formulário
-document.addEventListener('DOMContentLoaded',  function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Recupera os dados do localStorage
-    
     const nome = localStorage.getItem('nome');
     const apelido = localStorage.getItem('apelido');
     const email = localStorage.getItem('email');
@@ -31,51 +29,56 @@ document.addEventListener('DOMContentLoaded',  function() {
 });
 
 const form = document.getElementById('atualizarUsuarios');
- form.addEventListener('submit', async function(event) {
-     event.preventDefault(); // Impede o comportamento padrão do formulário
- 
-     const id = localStorage.getItem('id');
-     const nome = document.getElementById('nome-usuario').value;
-     const apelido = document.getElementById('apelido-usuario').value;
-     const email = document.getElementById('email-usuario').value;
-     const nascimento = document.getElementById('nascimento-usuario').value;
-     const file = document.getElementById('upload').files[0];
- 
-     const formData = new FormData();
-     formData.append('nome', nome);
-     formData.append('apelido', apelido);
-     formData.append('email', email);
-     formData.append('nascimento', nascimento);
-     if (file) {
-         formData.append('file', file);
-     }
- 
-     try {
-         const response = await fetch(`https://api-noob-1.onrender.com/api/usuarios/${id}`, {
-             method: 'PUT',
-             body: formData
-         });
- 
-         const responseData = await response.json();
- 
-         if (response.ok) {
-             alert('Usuário atualizado com sucesso!');
-             // Atualiza os dados no localStorage
-             localStorage.setItem('nome', responseData.usuario.nome);
-             localStorage.setItem('apelido', responseData.usuario.apelido);
-             localStorage.setItem('email', responseData.usuario.email);
-             localStorage.setItem('nascimento', responseData.usuario.nascimento);
-         } else {
-             alert(`Erro ao atualizar usuário: ${responseData.msg}`);
-         }
-     } catch (error) {
-         console.error('Erro:', error);
-         alert('Erro ao atualizar usuário.');
-     }
- });
+form.addEventListener('submit', async function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do formulário
 
+    const id = localStorage.getItem('id');
+    console.log('User ID:', id); // Verificar o ID
 
+    const nome = document.getElementById('nome-usuario').value;
+    const apelido = document.getElementById('apelido-usuario').value;
+    const email = document.getElementById('email-usuario').value;
+    const nascimento = document.getElementById('nascimento-usuario').value;
+    const file = document.getElementById('upload').files[0];
 
+    const formData = new FormData();
+    formData.append('nome', nome);
+    formData.append('apelido', apelido);
+    formData.append('email', email);
+    formData.append('nascimento', nascimento);
+    if (file) {
+        formData.append('file', file);
+    }
+
+    try {
+        const response = await fetch(`https://api-noob-1.onrender.com/api/usuarios/${id}`, {
+            method: 'PUT',
+            body: formData
+        });
+
+        const responseData = await response.json();
+        console.log('API Response:', responseData); // Log da resposta completa
+
+        if (response.ok) {
+            console.log('nome', responseData.usuario.nome);
+            console.log('apelido', responseData.usuario.apelido);
+            console.log('email', responseData.usuario.email);
+            console.log('nascimento', responseData.usuario.nascimento);
+
+            alert('Usuário atualizado com sucesso!');
+            // Atualiza os dados no localStorage
+            localStorage.setItem('nome', responseData.usuario.nome);
+            localStorage.setItem('apelido', responseData.usuario.apelido);
+            localStorage.setItem('email', responseData.usuario.email);
+            localStorage.setItem('nascimento', responseData.usuario.nascimento);
+        } else {
+            alert(`Erro ao atualizar usuário: ${responseData.msg}`);
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao atualizar usuário.');
+    }
+});
  
 
 
