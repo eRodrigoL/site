c28259058faadf6176d7ae4b2b47a6da429c6dc4
thais <thaismferreira42@gmail.com>
2024-06-18 18:29:39 -0300

    adicionando atualização de perfil

--- frontend/componentes/script-atualiza-usuario.js
+++ frontend/componentes/script-atualiza-usuario.js
@@ -43,6 +43,61 @@ async function carregarDadosUsuario() {
 document.addEventListener('DOMContentLoaded', carregarDadosUsuario());
 
 
+// atualização de perfil
+
+document.getElementById('atualizarUsuarios').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Evita o envio padrão do formulário
+
+    const id = localStorage.getItem('id');
+    const apiUrl =  `https://api-noob-1.onrender.com/api/usuarios/${id}`;
+
+   
+    // Captura os dados do formulário
+    const nome = document.getElementById('nome-usuario').value;
+    const apelido = document.getElementById('apelido-usuario').value;
+    const dataNascimento = document.getElementById('nascimento-usuario').value;
+    const email = document.getElementById('email-usuario').value;
+
+    // Cria o objeto com os dados do usuário
+    const usuario = {
+        nome: nome,
+        apelido: apelido,
+        nascimento: dataNascimento,
+        email: email
+    };
+
+    try {
+        // Faz a requisição PUT para a API
+        const response = await fetch(apiUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao atualizar usuário: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        alert('Usuário atualizado com sucesso!');
+        console.log('Usuário atualizado:', data);
+        carregarDadosUsuario();
+
+        
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao atualizar usuário. Por favor, tente novamente.');
+    }
+});
+
+
+
+
+
+
+
  
 
 
