d102aa963ab9c6a7a18c3d679fab42cdd469c76f
thais <thaismferreira42@gmail.com>
2024-06-18 18:10:27 -0300

    incluido carregamento de foto no perfil do usuário

--- frontend/componentes/script-atualiza-usuario.js
+++ frontend/componentes/script-atualiza-usuario.js
@@ -5,7 +5,8 @@ function formatarData(data) {
 
 async function carregarDadosUsuario() {
     const id = localStorage.getItem('id');
-    const src = localStorage.getItem('src');
+   // const src = localStorage.getItem('src');
+
     if (!id) {
         console.error('ID do usuário não encontrado no localStorage.');
         return;
@@ -27,10 +28,10 @@ async function carregarDadosUsuario() {
             const dataFormatada = formatarData(usuario.nascimento);
             document.getElementById('nascimento-usuario').value = dataFormatada;
         }
-
-        // Atualizar o src da imagem se a URL estiver disponível
-        if (src) {
-            document.getElementById('preview').src = src;
+        if(usuario.src){
+            const imgElement = document.getElementById('preview-img');
+            imgElement.src = usuario.src;
+    
         }
        
     } catch (error) {
@@ -39,77 +40,7 @@ async function carregarDadosUsuario() {
     }
 }
 
-// Executar a função quando o conteúdo do DOM estiver carregado
-document.addEventListener('DOMContentLoaded', carregarDadosUsuario);
-
-
-document.getElementById('atualizarUsuarios').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do formulário
-
-    const id = localStorage.getItem('id');
-    console.log('User ID:', id); // Verificar o ID
-
-    const nome = document.getElementById('nome-usuario').value;
-    const apelido = document.getElementById('apelido-usuario').value;
-    const email = document.getElementById('email-usuario').value;
-    const nascimento = document.getElementById('nascimento-usuario').value;
-    const file = document.getElementById('upload').files[0];
-    
-
-    const formData = new FormData();
-    formData.append('nome', nome);
-    formData.append('apelido', apelido);
-    formData.append('email', email);
-    formData.append('nascimento', nascimento);
-    if (file) {
-        formData.append('file', file);
-    }
-
-    try {
-        const response = await fetch(`https://api-noob-1.onrender.com/api/usuarios/${id}`, {
-            method: 'PUT',
-            body: formData
-        });
-
-        const responseData = await response.json();
-        console.log('API Response:', responseData); // Log da resposta completa
-
-        if (response.ok) {
-            console.log('nome', responseData.usuario.nome);
-            console.log('apelido', responseData.usuario.apelido);
-            console.log('email', responseData.usuario.email);
-            console.log('nascimento', responseData.usuario.nascimento);
-
-            alert('Usuário atualizado com sucesso!');
-            // Atualiza os dados no localStorage
-           /* localStorage.setItem('nome', responseData.usuario.nome);
-            localStorage.setItem('apelido', responseData.usuario.apelido);
-            localStorage.setItem('email', responseData.usuario.email);
-            localStorage.setItem('nascimento', responseData.usuario.nascimento);*/
-        } else {
-            alert(`Erro ao atualizar usuário: ${responseData.msg}`);
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-        alert('Erro ao atualizar usuário.');
-    }
-});
-
-document.getElementById('upload').onchange = function (evt) {
-    var tgt = evt.target || window.event.srcElement,
-        files = tgt.files;
-
-    // Verifica se foi selecionada alguma imagem
-    if (FileReader && files && files.length) {
-        var fr = new FileReader();
-        fr.onload = function () {
-            var img = document.getElementById('preview');
-            img.src = fr.result;
-            img.style.display = 'block'; // Exibe a imagem
-        }
-        fr.readAsDataURL(files[0]);
-    }
-};
+document.addEventListener('DOMContentLoaded', carregarDadosUsuario());
 
 
  
